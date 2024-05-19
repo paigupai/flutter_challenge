f088973a05067b09ca4ebf67ccd0e0080c279990
paigupai <44311361+paigupai@users.noreply.github.com>
2024-05-19 16:12:46 +0900

    不要code削除

--- lib/main.dart
+++ lib/main.dart
@@ -55,24 +55,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    const tokyoStation = CameraPosition(target: LatLng(35.681236, 139.767125));
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: const GoogleMap(
-        initialCameraPosition: tokyoStation,
-      ),
-    );
-  }
-}
